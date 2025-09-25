cb34fd245c0506d6bbde78659017e65b0f857119
damian21012 <mlgk3441@naver.com>
2025-09-25 16:41:17 +0900

    수정

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,22 @@ import { useState } from 'react'
 function App() {
   const [count1, setCount1] = useState(0)
   const [count2, setCount2] = useState(0)
+
   return (
     <div>
       <h1>총합: {count1 + count2}</h1>
-      <Counter
-      count={count1}
-      onIncrement={
-        () => setCount(prev => prev + 1)} />
-
-      <Counter
-      count={count2}
-      onIncrement={
-        () => setCount(prev => prev + 1)} />
+      <Counter count={count1} onIncrement={() => setCount1(prev => prev + 1)} />
+      <Counter count={count2} onIncrement={() => setCount2(prev => prev + 1)} />
     </div>
   )
 }
 
-function Counter({ count, onIncrement}) {
-
+function Counter({ count, onIncrement }) {
   return (
     <div>
-      <h1>Counter: {counter}</h1>
-      <button 
-        onClick={ onIncrement }>
-          증가
+      <h1>Counter: {count}</h1>
+      <button onClick={onIncrement}>
+        증가
       </button>
     </div>
   )
